87b2c607c460400e1711da1c8cdf0aaf3c43350d
manha <manha@lti04.cesed.local>
2017-10-17 11:18:36 -0200

    atualizando AvaliacoesPage

--- src/pages/avaliacoes/avaliacoes.ts
+++ src/pages/avaliacoes/avaliacoes.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { AvaliacaoParqPage } from "./avaliacao-parq/avaliacao-parq";
-import { AvaliacaoCoronarianoPage } from "./avaliacao-coronariano/avaliacao-coronariano";
-import { AvaliacaoFisicaPage } from "./avaliacao-fisica/avaliacao-fisica";
-import { AvaliacaoRiscoPage } from "./avaliacao-risco/avaliacao-risco";
 
 import { AvaliacaoParqPage } from "./avaliacao-parq/avaliacao-parq";
 import { AvaliacaoRiscoPage } from "./avaliacao-risco/avaliacao-risco";
@@ -17,33 +13,17 @@ import { AvaliacaoFisicaPage } from "./avaliacao-fisica/avaliacao-fisica";
 export class AvaliacoesPage {
   selectedItem: any;
   icons: string[];
-<<<<<<< HEAD
-  items: Array<{title: string, icon: string, page: any}>;
-=======
-  items: Array<{title: string, icon: string, page:any}>;
->>>>>>> fd85a8dc57acb6eb5ffb0926bde5daf42d0a339c
 
+  items: Array<{title: string, icon: string, page: any}>;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.selectedItem = navParams.get('item');
     
-<<<<<<< HEAD
     this.items = [{title:'Questionário PAR-Q', icon:'paper', page: AvaliacaoParqPage}, 
                   {title:'Índice de Risco Cardíaco', icon:'paper',page: AvaliacaoRiscoPage},
                   {title:'Avaliação dos Fatores de Risco Coronariano', icon:'paper',page: AvaliacaoCoronarianoPage},
                   {title:'Avaliação do Nível de Atividade Física', icon:'paper',page: AvaliacaoFisicaPage}];
-=======
-    this.items = [{title:'Questionário PAR-Q', icon:'paper',page: AvaliacaoParqPage}, 
-                  {title:'Índice de Risco Cardíaco', icon:'paper',page: AvaliacaoRiscoPage},
-                  {title:'Avaliação dos Fatores de Risco Coronariano', icon:'paper', page: AvaliacaoCoronarianoPage},
-                  {title:'Avaliação do Nível de Atividade Física', icon:'paper', page: AvaliacaoFisicaPage}];
->>>>>>> fd85a8dc57acb6eb5ffb0926bde5daf42d0a339c
   }
   itemTapped(event, item) {
-<<<<<<< HEAD
     this.navCtrl.push(item.page, {item: item});
-=======
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(item.page);
->>>>>>> fd85a8dc57acb6eb5ffb0926bde5daf42d0a339c
   }
 }
