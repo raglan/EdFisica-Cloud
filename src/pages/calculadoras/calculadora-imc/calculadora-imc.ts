6c30c08661ee506b01141467835754a86332fdd4
Oliver de Castro mendoza <olivercastro2002@gmail.com>
2017-10-23 12:08:59 -0300

    imc funcionando

--- src/pages/calculadoras/calculadora-imc/calculadora-imc.ts
+++ src/pages/calculadoras/calculadora-imc/calculadora-imc.ts
@@ -1,25 +1,73 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
 /**
  * Generated class for the CalculadoraImcPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+class Imc{
+  altura:number;   
+  peso:number;
+  sexo:String;
+}
 @IonicPage()
 @Component({
   selector: 'page-calculadora-imc',
   templateUrl: 'calculadora-imc.html',
 })
 export class CalculadoraImcPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  imc:Imc
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    this.imc = new Imc()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CalculadoraImcPage');
   }
+  showConfirm(frase:string) {
+    let confirm = this.alertCtrl.create({
+      title: 'IMC ',
+      message: frase,
+      buttons: [
+        
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 
+  calcula_imc(){
+
+    var quadrado = (this.imc.altura * this.imc.altura);
+    
+    var calculoP = (this.imc.peso/quadrado);
+    
+    var calculo = parseFloat(calculoP.toFixed(1))
+
+    if(calculo<18.5){
+      this.showConfirm("Você está magro com esse indice:"  + calculo); 
+    }
+    else if(calculo>=18.5 && calculo<24.9){
+      this.showConfirm("Você está normal com esse indice: " + calculo);
+    }
+    
+    else if(calculo>=25 && calculo<29.9) {
+      this.showConfirm("Você está com sobre peso com esse indice: " + calculo);
+    }
+    else if(calculo>=30 && calculo<39.9) {
+      this.showConfirm("Você está com obesidade com esse indice: " + calculo);
+    }
+    else if (calculo>40){
+      this.showConfirm("Você estácom obesidade grave com esse indice: " + calculo);
+    }
+    this.imc = new Imc()
+  }
+  
 }
