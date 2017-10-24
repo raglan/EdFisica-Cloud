5eeb161582c315c9ceb05deeb1626b46ebe15749
Oliver de Castro mendoza <olivercastro2002@gmail.com>
2017-10-24 11:08:24 -0300

    ajuste calcimc

--- src/pages/calculadoras/calculadora-imc/calculadora-imc.ts
+++ src/pages/calculadoras/calculadora-imc/calculadora-imc.ts
@@ -10,7 +10,7 @@ import { AlertController } from 'ionic-angular';
 class Imc{
   altura:number;   
   peso:number;
-  sexo:String;
+  sexo;
 }
 @IonicPage()
 @Component({
@@ -51,23 +51,45 @@ export class CalculadoraImcPage {
     
     var calculo = parseFloat(calculoP.toFixed(1))
 
-    if(calculo<18.5){
-      this.showConfirm("Você está magro com esse indice:"  + calculo); 
+    if(this.imc.sexo == "masc"){
+          if(calculo<20.0){
+            this.showConfirm("Você está magro com esse indice: "  + calculo); 
+          }
+          else if(calculo>=20.0 && calculo<24.9){
+            this.showConfirm("Você está normal com esse indice: " + calculo);
+          }
+          
+          else if(calculo>=25 && calculo<29.9) {
+            this.showConfirm("Você está com sobre peso com esse indice: " + calculo);
+          }
+          else if(calculo>=30 && calculo<39.9) {
+            this.showConfirm("Você está com obesidade com esse indice: " + calculo);
+          }
+          else if (calculo>43){
+            this.showConfirm("Você estácom obesidade grave com esse indice: " + calculo);
+          }
+          this.imc = new Imc()
     }
-    else if(calculo>=18.5 && calculo<24.9){
-      this.showConfirm("Você está normal com esse indice: " + calculo);
+    else if(this.imc.sexo == "fem"){
+            if(calculo<19.0){
+              this.showConfirm("Você está magra com esse indice: "  + calculo); 
+            }
+            else if(calculo>=19.0 && calculo<23.9){
+              this.showConfirm("Você está normal com esse indice: " + calculo);
+            }
+            
+            else if(calculo>=24 && calculo<28.9) {
+              this.showConfirm("Você está com sobre peso com esse indice: " + calculo);
+            }
+            else if(calculo>=29 && calculo<38.9) {
+              this.showConfirm("Você está com obesidade com esse indice: " + calculo);
+            }
+            else if (calculo>39){
+              this.showConfirm("Você estácom obesidade grave com esse indice: " + calculo);
+            }
+            this.imc = new Imc()
+          }
     }
-    
-    else if(calculo>=25 && calculo<29.9) {
-      this.showConfirm("Você está com sobre peso com esse indice: " + calculo);
-    }
-    else if(calculo>=30 && calculo<39.9) {
-      this.showConfirm("Você está com obesidade com esse indice: " + calculo);
-    }
-    else if (calculo>40){
-      this.showConfirm("Você estácom obesidade grave com esse indice: " + calculo);
-    }
-    this.imc = new Imc()
-  }
-  
+
+   
 }
