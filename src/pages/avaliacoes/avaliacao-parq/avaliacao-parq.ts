11b3f569908e7797efc63ff33f67e6aeb197bb00
Oliver de Castro mendoza <olivercastro2002@gmail.com>
2017-10-31 11:30:06 -0300

    avali parq

--- src/pages/avaliacoes/avaliacao-parq/avaliacao-parq.ts
+++ src/pages/avaliacoes/avaliacao-parq/avaliacao-parq.ts
@@ -1,31 +1,38 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {  FormGroup,  FormControl} from '@angular/forms';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+
 /**
  * Generated class for the AvaliacaoParqPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+class Parq {
+  opcao1;
+  opcao2;
+  opcao3;
+  opcao4;
+  opcao5;
+  opcao6;
+  opcao7;
+}
 @IonicPage()
 @Component({
   selector: 'page-avaliacao-parq',
   templateUrl: 'avaliacao-parq.html',
 })
 export class AvaliacaoParqPage {
+  parq: Parq
+  opcao: string;
   langs;
   langForm;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.langForm = new FormGroup({
-      "langs": new FormControl({value: 'rust', disabled: false})
-    });
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+
+    this.parq= new Parq()
+    
   }
-  doSubmit(event) {
-    console.log('Submitting form', this.langForm.value);
-    event.preventDefault();
-  }
+  
     
 
 
@@ -34,7 +41,44 @@ export class AvaliacaoParqPage {
     ionViewDidLoad() {
       console.log('ionViewDidLoad AvaliacaoParqPage');
     }
+    showConfirm(frase:string) {
+      let confirm = this.alertCtrl.create({
+        title: 'PAR-Q ',
+        message: frase,
+        buttons: [
+          
+          {
+            text: 'OK',
+            handler: () => {
+              console.log('Agree clicked');
+            }
+          }
+        ]
+      });
+      confirm.present();
+    }
   
+    calcula_parq(){
+          if(this.parq.opcao1 == "sim" ||
+            this.parq.opcao2 == "sim" ||
+            this.parq.opcao3 == "sim" ||
+            this.parq.opcao4 == "sim" ||
+            this.parq.opcao5 == "sim" ||
+            this.parq.opcao6 == "sim" ||
+            this.parq.opcao7 == "sim"){
+            
+              this.showConfirm(" Você deve procurar um médico ! ");
+          } else{
+              this.showConfirm(" Pode prosseguir ");
+          }
+          // if(this.parq.opcao1 == "nao"||this.parq.opcao2 == "nao"||this.parq.opcao3 == "nao"||this.parq.opcao4 == "nao"
+          // ||this.parq.opcao5 == "nao"||this.parq.opcao6 == "nao"||this.parq.opcao7 == "nao"){
+          
+          // this.showConfirm(" Pode prosseguir ");
+          //}
+        
+          // this.parq = new Parq()
+    }
 
 }
 
