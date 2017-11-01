d4f355f9b126510e3919a801705f7e582f6f27b2
Oliver de Castro mendoza <olivercastro2002@gmail.com>
2017-11-01 11:12:06 -0300

    1 rm

--- src/pages/calculadoras/calculadora-1rm/calculadora-1rm.ts
+++ src/pages/calculadoras/calculadora-1rm/calculadora-1rm.ts
@@ -1,17 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+class Umrm{
+  peso:number;
+  Max: number;
+}
 @Component({
   selector: 'page-calculadora-1rm',
   templateUrl: 'calculadora-1rm.html',
 })
 export class Calculadora1RmPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  umrm:Umrm;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+    this.umrm = new Umrm();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Calculadora1RmPage');
+  }
+  showConfirm(frase:string) {
+    let confirm = this.alertCtrl.create({
+      title: '1 Rm ',
+      message: frase,
+      buttons: [
+        
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  calcula_umrm(){
+        
   }
 
 }
