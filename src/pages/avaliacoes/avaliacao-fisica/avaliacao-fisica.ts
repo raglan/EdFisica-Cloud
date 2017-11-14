690953b03de6dece733d29c5f807d07ca017c975
Oliver de Castro mendoza <olivercastro2002@gmail.com>
2017-11-14 10:49:56 -0300

    avaliacao risco

--- src/pages/avaliacoes/avaliacao-fisica/avaliacao-fisica.ts
+++ src/pages/avaliacoes/avaliacao-fisica/avaliacao-fisica.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the AvaliacaoFisicaPage page.
@@ -7,19 +7,62 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+class AvaliacaoFisica{
+  quest1:number;
+  quest2:number;
+  quest3:number;
+  
 
+}
 @IonicPage()
 @Component({
   selector: 'page-avaliacao-fisica',
   templateUrl: 'avaliacao-fisica.html',
 })
 export class AvaliacaoFisicaPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  avaliacaoFisica: AvaliacaoFisica;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+    this.avaliacaoFisica= new AvaliacaoFisica()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AvaliacaoFisicaPage');
   }
+  showConfirm(frase:string) {
+    let confirm = this.alertCtrl.create({
+      title: 'Avaliação Física ',
+      message: frase,
+      buttons: [
+        
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+    }
+    calcula_avaliacao(){
+      
+      var somaPontos = ((this.avaliacaoFisica.quest1 * 1) + (this.avaliacaoFisica.quest2 * 1) 
+                          + (this.avaliacaoFisica.quest3 * 1))
+                
+       
+                
+  
+      if((somaPontos >= 1) && (somaPontos < 2)){
+      this.showConfirm("Muito Pobre: " + somaPontos + " Sedentário");
+      }else if((somaPontos >= 2) && (somaPontos < 5)){
+        this.showConfirm("Pobre: " + somaPontos + " Insuficiente");
+      }else if((somaPontos >= 4) && (somaPontos < 7)){
+      this.showConfirm("Razoável: " + somaPontos + " Aceitável - deve melhorar ");
+      }else if((somaPontos >= 6) && (somaPontos < 9)){
+        this.showConfirm("Médio: " + somaPontos + " Aceitável ");
+      }else if( (somaPontos >= 8) && (somaPontos < 11)){
+          this.showConfirm("Ótimo: " + somaPontos + " Estilo de Vida Muito Ativo ");
+      }
+    }
 
 }
--- src/pages/avaliacoes/avaliacao-risco/avaliacao-risco.ts
+++ src/pages/avaliacoes/avaliacao-risco/avaliacao-risco.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the AvaliacaoRiscoPage page.
@@ -7,19 +7,72 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+class AvaliacaoRisco{
+  opcao1:number;
+  opcao2:number;
+  opcao3:number;
+  opcao4:number;
+  opcao5:number;
+  opcao6:number;
+  opcao7:number;
+  opcao8:number;
+  
 
+}
 @IonicPage()
 @Component({
   selector: 'page-avaliacao-risco',
   templateUrl: 'avaliacao-risco.html',
 })
 export class AvaliacaoRiscoPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  avaliacaoRisco : AvaliacaoRisco
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
+    this.avaliacaoRisco= new AvaliacaoRisco()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AvaliacaoRiscoPage');
   }
+  showConfirm(frase:string) {
+    let confirm = this.alertCtrl.create({
+      title: ' Risco Cardíaco (RISKO)  ',
+      message: frase,
+      buttons: [
+        
+        {
+          text: 'OK',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+    }
+
+  calcula_risco(){
+         
+      var somaPontos = ((this.avaliacaoRisco.opcao1 * 1) + (this.avaliacaoRisco.opcao2 * 1) + (this.avaliacaoRisco.opcao3 * 1)
+      + (this.avaliacaoRisco.opcao4 * 1) + (this.avaliacaoRisco.opcao5 * 1) + (this.avaliacaoRisco.opcao6 * 1)
+    + (this.avaliacaoRisco.opcao7 * 1) + (this.avaliacaoRisco.opcao8 * 1))
+      
+      
+                
+
+      if((somaPontos >= 6) && (somaPontos < 12)){
+      this.showConfirm("Total: " + somaPontos + " Risco bem baixo da média ");
+      }else if((somaPontos >= 12) && (somaPontos < 18)){
+        this.showConfirm("Total: " + somaPontos + " Risco abaixo da média ");
+      }else if((somaPontos >= 18) && (somaPontos < 25)){
+      this.showConfirm("Total: " + somaPontos + " Risco na média geral ");
+      }else if((somaPontos >= 25) && (somaPontos < 35)){
+        this.showConfirm("Total: " + somaPontos + " Risco moderado ");
+      }else if( (somaPontos >= 35) && (somaPontos < 41)){
+          this.showConfirm("Total: " + somaPontos + " Risco no nível perigoso ");
+      }else if( (somaPontos >= 41) && (somaPontos < 62)){
+      this.showConfirm("Total: " + somaPontos + " Perigo urgente. Consulte seu médico ");
+      }
+  }
+
 
 }
